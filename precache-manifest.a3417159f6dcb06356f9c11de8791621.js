3e70d1a434035594021ec400b95cd61b9f21bf98
Jason Fritsche <fritsche.jason@gmail.com>
2019-02-06 14:02:35 -0600

    Updates

---
+++ precache-manifest.a3417159f6dcb06356f9c11de8791621.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20da10eb2857853dc5b8",
+    "url": "/BreweryFinder/static/js/runtime~main.20da10eb.js"
+  },
+  {
+    "revision": "faf98103f6ec3fc2c4e2",
+    "url": "/BreweryFinder/static/js/main.faf98103.chunk.js"
+  },
+  {
+    "revision": "e7299eefb4df27f337da",
+    "url": "/BreweryFinder/static/js/1.e7299eef.chunk.js"
+  },
+  {
+    "revision": "faf98103f6ec3fc2c4e2",
+    "url": "/BreweryFinder/static/css/main.c06f4691.chunk.css"
+  },
+  {
+    "revision": "3b7769d1d886b2f0ca4186ea20c64511",
+    "url": "/BreweryFinder/index.html"
+  }
+];
