3a4701f6b3ac0de7591bb2b133840e6165778206
Jason Fritsche <34847680+JasonFritsche@users.noreply.github.com>
2019-02-28 15:37:55 -0600

    removed jQuery as a dependency, removed jQuery code from Brewery.js (#19)

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
-import $ from 'jquery'
 
 export default class Brewery extends Component {
-
-  componentDidMount(){
-    $(".btn").mouseup(function(){
-      $(this).blur();
-  })
-  }
-  
   displayWebsiteUrl() {
     if (this.props.item.website_url === "") {
       return;
@@ -24,7 +16,7 @@ export default class Brewery extends Component {
         </a>
       );
     }
-  };
+  }
 
   render() {
     return (
