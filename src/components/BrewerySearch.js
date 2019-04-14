8e5c9ddca463f00a9a1281e2b042303efb861c29
Rocktim Saikia <srocktim61@gmail.com>
2019-04-14 14:17:06 -0700

    removed old fonts ,moved body section css to index.css, classed instead of ids,import instead of require

--- src/components/BrewerySearch.js
+++ src/components/BrewerySearch.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 import Typed from 'react-typed'
 import * as Alert from 'react-bootstrap/Alert'
 import RadioButtonContainer from './RadioButtonContainer'
-const img = require('../public/cheers.png')
-const githublogo = require('../public/github-logo.png')
+import img from '../public/cheers.png'
+import githublogo from '../public/github-logo.png'
 
 export default class BrewerySearch extends Component {
   state = {
@@ -50,8 +50,8 @@ export default class BrewerySearch extends Component {
           <div className="row">
             <div className="col">
               <h1 className="Quicksand-Text Glow text-center">
-                <img src={img} id="vector-img" />{' '}
-                <span id="underline">Brew</span>ery Finder
+                <img src={img} className="vector-img" alt="beer-vector" />{' '}
+                <span className="underline">Brew</span>ery Finder
               </h1>
             </div>
           </div>
@@ -79,7 +79,7 @@ export default class BrewerySearch extends Component {
         <div className="container mb-5">
           <div className="row">
             <div className="col-09 mx-auto col-md-8 mt-6 text-center">
-              <h2 className="text-capitalize" id="sub-text">
+              <h2 className="text-capitalize">
                 search for breweries by {searchFilter}
               </h2>
               <form className="form input-group" onSubmit={this.onSearchClick}>
@@ -100,9 +100,6 @@ export default class BrewerySearch extends Component {
                 </Alert>
               )}
             </div>
-            {/* <div className="col-md-4">
-              <img id="beer-vector" src={img} />
-            </div> */}
           </div>
           <div className="row">
             <div className="radio-button form-check-inline">
@@ -134,15 +131,6 @@ export default class BrewerySearch extends Component {
                 identifier="typeRadio"
                 tooltip="Types: micro, regional, brewpub, large, planning, bar, contract, proprietor"
               />
-
-              {/* commenting out the tag radio  button for now, as the API doesn't offer much support for this feature yet. Uncomment and test it yourself to see if more search results appear. If so, feel free to uncomment and commit the code to reintroduce the 'tag' radio button */}
-              {/* <RadioButtonContainer
-                val="tag"
-                searchBy={this.state.searchBy}
-                handleSearchChange={this.onSearchChange}
-                identifier="tagRadio"
-                tooltip="Example: dog-friendly, patio, food-service, food-truck, tours"
-              /> */}
             </div>
           </div>
         </div>
@@ -153,7 +141,7 @@ export default class BrewerySearch extends Component {
               className="d-block"
               target="_blank"
             >
-              Contribute <img src={githublogo} id="logo" />
+              Contribute <img src={githublogo} id="logo" alt="github-logo" />
             </a>
             Powered By{' '}
             <span className="text-muted">
