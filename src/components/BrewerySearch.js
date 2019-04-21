dde90ca029b4a31405646dc0e5cd98e463a3f38d
Charles M Drani <charlesdrani@gmail.com>
2019-04-20 18:34:16 -0600

    remove unnecessary div elements

--- src/components/BrewerySearch.js
+++ src/components/BrewerySearch.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Typed from 'react-typed'
 import * as Alert from 'react-bootstrap/Alert'
-import Footer from './Footer'
 
-import RadioButtonContainer from './RadioButtonContainer'
+import Footer from './Footer'
 import img from '../public/cheers.png'
+import RadioButtonContainer from './RadioButtonContainer'
 
 export default class BrewerySearch extends Component {
   state = {
@@ -68,34 +68,32 @@ export default class BrewerySearch extends Component {
     const { searchFilter, showAlert } = this.state
     return (
       <>
-        <div>
-          <div className="row">
-            <div className="col">
-              <h1 className="Quicksand-Text Glow text-center">
-                <img src={img} className="vector-img" alt="beer-vector" />{' '}
-                <span className="underline">Brew</span>ery Finder
-              </h1>
-            </div>
+        <div className="row">
+          <div className="col">
+            <h1 className="Quicksand-Text Glow text-center">
+              <img src={img} className="vector-img" alt="beer-vector" />{' '}
+              <span className="underline">Brew</span>ery Finder
+            </h1>
           </div>
-          <div className="row">
-            <div className="col text-center mb-5">
-              <Typed
-                strings={[
-                  'Find a brewery in your hometown',
-                  'Find your new hangout',
-                  'Find your new favorite beer',
-                  'Find the answer to your problems'
-                ]}
-                typeSpeed={70}
-                startDelay={1200}
-                backDelay={3000}
-                backSpeed={60}
-                loop
-                loopCount={30}
-                showCursor
-                className="typing-text"
-              />
-            </div>
+        </div>
+        <div className="row">
+          <div className="col text-center mb-5">
+            <Typed
+              strings={[
+                'Find a brewery in your hometown',
+                'Find your new hangout',
+                'Find your new favorite beer',
+                'Find the answer to your problems'
+              ]}
+              typeSpeed={70}
+              startDelay={1200}
+              backDelay={3000}
+              backSpeed={60}
+              loop
+              loopCount={30}
+              showCursor
+              className="typing-text"
+            />
           </div>
         </div>
         <div className="container mb-5">
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,20 +3,18 @@ import Anchor from './Anchor'
 import githublogo from '../public/github-logo.png'
 
 const Footer = () => (
-  <div>
-    <footer className="mx-auto py-3">
-      <div className="container text-center">
-        <Anchor
-          url="https://github.com/JasonFritsche/BreweryFinder"
-          classes="d-block"
-        >
-          Repo <img src={githublogo} id="logo" alt="github-logo" />
-        </Anchor>
-        Powered By{' '}
-        <Anchor url="https://www.openbrewerydb.org">Open Brewery DB</Anchor>
-      </div>
-    </footer>
-  </div>
+  <footer className="mx-auto py-3">
+    <div className="container text-center">
+      <Anchor
+        url="https://github.com/JasonFritsche/BreweryFinder"
+        classes="d-block"
+      >
+        Repo <img src={githublogo} id="logo" alt="github-logo" />
+      </Anchor>
+      Powered By{' '}
+      <Anchor url="https://www.openbrewerydb.org">Open Brewery DB</Anchor>
+    </div>
+  </footer>
 )
 
 export default Footer
