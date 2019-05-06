aee2dfbb2c2ab6d223b372676139700d969ce1be
prashkup <prashanthkuppa333@gmail.com>
2019-05-06 20:02:42 +0530

    Inline the footer elems on tablets, larger screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,17 @@ import githublogo from '../public/github-logo.png'
 
 const Footer = () => (
   <footer className="mx-auto py-3">
-    <div className="container text-center">
+    <div className="container text-center footer-content">
       <Anchor
         url="https://github.com/JasonFritsche/BreweryFinder"
-        classes="d-block"
+        classes="d-block footer-content-item"
       >
         Repo <img src={githublogo} id="logo" alt="github-logo" />
       </Anchor>
       Powered By{' '}
-      <Anchor url="https://www.openbrewerydb.org">Open Brewery DB</Anchor>
+      <Anchor url="https://www.openbrewerydb.org" classes="footer-content-item">
+        Open Brewery DB
+      </Anchor>
     </div>
   </footer>
 )
