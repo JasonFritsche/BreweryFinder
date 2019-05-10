634aca504919582cbe39be0314ca8d53fffe22d7
cdrani <charlesdrani@gmail.com>
2019-05-10 13:21:56 -0600

    test(Anchor.js): add test to confirm render

---
+++ src/__tests__/Anchor.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+
+import Anchor from '../components/Anchor'
+
+describe('Anchor', () => {
+  const children = <h2 data-testid="header">https://beer.me</h2>
+
+  const props = {
+    testId: 'header',
+    url: 'https://beer.me',
+    classes: 'class1 class2',
+    children
+  }
+
+  it('renders the component', () => {
+    const { getByTestId } = render(<Anchor {...props} />)
+    const header = getByTestId(props.testId)
+    expect(header.innerHTML).toBe(props.url)
+  })
+})
